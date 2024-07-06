11799f99db546ac6e74fd3dd36f9689ba1fb25b0
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-06 23:13:39 +0200

    Implemented code

---
+++ chat_client.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+import tkinter as tkt
+
+def receive():
+    while True:
+        try:
+            msg = client_socket.recv(BUFSIZE).decode("utf8")
+            msg_list.insert(tkt.END, msg )
+        except OSError:
+            break
+        
+def send(event=None):
+    msg = my_msg.get()
+    my_msg.set("")
+    client_socket.send(bytes(msg, "utf8"))
+    if msg == "{quit}":
+        client_socket.close()
+        window.quit()
+        
+        
+def on_closing(event=None):
+    my_msg.set("{quit}")
+    send()
+    
+#setting up GUI
+window = tkt.Tk()
+window.title("Chat")
+message_frame = tkt.Frame(window)
+my_msg = tkt.StringVar()
+my_msg.set("Write your messages here ! ")
+scrollbar = tkt.Scrollbar(message_frame)
+msg_list = tkt.Listbox(message_frame, height=20, width=70, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=tkt.RIGHT, fill=tkt.Y)
+msg_list.pack(side=tkt.LEFT, fill=tkt.BOTH)
+msg_list.pack()
+message_frame.pack
+entry_field = tkt.Entry(window, textvariable=my_msg)
+entry_field.bind("<Return>", send)
+entry_field.pack()
+send_button = tkt.Button(window, text="Send", command=send)
+send_button.pack()
+window.protocol("WM_DELETE_WINDOW", on_closing)
+    
+HOST = input("Inserire l'indirizzo dell'host: ")
+PORT = input("Inserire la porta dell'host: ")
+if not PORT:
+    PORT = 5300
+else:
+    PORT = int(PORT)
+    
+BUFSIZE = 1024
+ADDR = (HOST,PORT)
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(ADDR)
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
+
+tkt.mainloop()
+
--- chat_server.py
+++ chat_server.py
@@ -4,15 +4,15 @@ from threading import Thread
 def accept_connection():
     while True:
         client,client_adress = SERVER.accept()
-        print("%s: %s si è collegato." % client)
+        print("%s: %s si è collegato." % client_adress)
         indirizzi[client] = client_adress
         Thread(target=handle_client, args=(client,)).start()
         
 def handle_client(client):
     nome = client.recv(BUFSIZ).decode("utf8")
-    benvenuto = "Benvenuto %s: se vuoi lasciare la chat, digita {quit} " %nome
+    benvenuto = "Benvenuto %s: se vuoi lasciare la chat, digita {quit} " % nome
     client.send(bytes(benvenuto,"utf8"))
-    msg_br = "Utente %s si è unito alla chat " %nome
+    msg_br = "Utente %s si è unito alla chat " % nome
     broadcast(bytes(msg_br, "utf8"))
     clients[client] = nome
     
